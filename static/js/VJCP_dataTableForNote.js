7035cd4667551a3973ef0420b7123c1506836de2
Kampanman <55080133+Kampanman@users.noreply.github.com>
2022-12-17 17:17:14 +0900

    Fixed  registration ID 1 can also be deleted
    Fixed a problem that notes and videos with registration ID 1 can also be deleted

--- static/js/VJCP_dataTableForNote.js
+++ static/js/VJCP_dataTableForNote.js
@@ -29,7 +29,7 @@
       >
         <template v-slot:item.editAndDelete="{ item }">
           <v-icon small class="mr-2" @click="editItem(item)">mdi-pencil</v-icon>
-          <v-icon small @click="deleteItem(item)">mdi-delete</v-icon>
+          <v-icon small v-if="item.id!=1" @click="deleteItem(item)">mdi-delete</v-icon>
         </template>
       </v-data-table>
     </v-card>
@@ -54,7 +54,7 @@
           <v-text-field label="URL" placeholder="URLを入力してください" v-model="selectItem.url"></v-text-field>
           <div style="margin:5px;width:180px;">
             <v-app>
-              <v-select label="公開設定" :items="pubStrs" v-model="selectItem.pub_str"></v-select>
+              <v-select label="公開設定" :items="pubStrs" v-if="selectItem.id!=1" v-model="selectItem.pub_str"></v-select>
             </v-app>
           </div>
           <div>
